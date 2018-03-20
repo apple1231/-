644470057577710e93989612fb87cb531e9f9bcb
apple1231 <33191711+apple1231@users.noreply.github.com>
2018-03-20 18:26:25 +0900

    Initial Commit

---
+++ 사칙연산 계산기/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  사칙연산 계산기
+//
+//  Created by 손종인 on 2018. 3. 20..
+//  Copyright © 2018년 손종인. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
